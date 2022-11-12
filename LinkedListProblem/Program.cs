17cbc632d1d3d5a599e7e81ecda09dc22ecc32ae
rahul01r <rrdubeynibm@gmail.com>
2022-11-12 09:14:06 +0530

    [rahul] Add:UC1-CreateALinkedList

---
+++ LinkedListProblem/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace LinkedListProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Select Programs\n 1.Create LinkedList");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        list.Add(56);
+                        list.Add(30);
+                        list.Add(70);
+                        list.Display();
+                        break;
+
+                }
+            }
+        }
+    }
+}
